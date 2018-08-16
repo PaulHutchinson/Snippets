9bedafc4d42fcb0241a474d71a0f984e3d71b32e
PaulHutchinson <xbsolutions@gmail.com>
2018-08-16 20:23:16 +0100

    Update TideTimes.py
    Corrected data source reference

--- TideTimes.py
+++ TideTimes.py
@@ -11,9 +11,9 @@ import datetime
 import time
 
 
-# Function to return sea height from met office monitored location
+# Function to return sea height from Environment Agency Tide Gauge API
 def getSeaHeight(time):
-	# Metoffice API is slow to update so subtract 2hrs from now
+	# API source data is slow to update so subtract 2hrs from now
 	nowTime = time - datetime.timedelta(hours=2)
 	# Round measurement to floor 15 minutes (Sample T of Tide Times)
 	#nowTime += datetime.timedelta(minutes=15)
