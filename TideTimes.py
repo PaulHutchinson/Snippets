ced2598b059a2d6181ed5868ec0df5ddeb2d0b6d
PaulHutchinson <xbsolutions@gmail.com>
2018-08-16 20:19:22 +0100

    Create TideTimes.py
    Core function

---
+++ TideTimes.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 16 17:14:13 2018
+Python script to extract sea hights from location E72539
+@author: phutchinson
+"""
+
+import requests
+import json
+import datetime
+import time
+
+
+# Function to return sea height from met office monitored location
+def getSeaHeight(time):
+	# Metoffice API is slow to update so subtract 2hrs from now
+	nowTime = time - datetime.timedelta(hours=2)
+	# Round measurement to floor 15 minutes (Sample T of Tide Times)
+	#nowTime += datetime.timedelta(minutes=15)
+	nowTime -= datetime.timedelta(minutes=nowTime.minute % 15)
+	nowTimeFormatMetApi = nowTime.strftime("%Y-%m-%dT%H-%M-00Z")
+	#print(nowTimeFormatMetApi)
+
+	response = requests.get(
+		"http://environment.data.gov.uk/flood-monitoring/data/readings/E72539-level-tidal_level-Mean-15_min-mAOD/{0}".
+		format(nowTimeFormatMetApi))
+
+	# Parse JSON reponse for sea height (Meters)
+	if response.status_code == 200:
+		parsed_json = json.loads(response.content.decode('utf-8'))
+		#print(parsed_json['items']['dateTime'])
+		#print(parsed_json['items']['value'])
+		return parsed_json['items']['value'], parsed_json['items']['dateTime']
+	else:
+		#print("Err. no data available")
+		return "Err. no data available"
+
+
+seaHeight, sampleDateTime = getSeaHeight(datetime.datetime.now())
+print(seaHeight)
+print(sampleDateTime)
